21c5d69f32ec376837f71fafa7cebc10a75baa45
Andrew Koller <arellok@gmail.com>
2018-03-13 17:34:16 -0700

    Added environment config

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
 # stdlib
+from os import environ as env
 
 # third-party
 
 # local
+import load_env
 
 
 class Config():
     """
     Class to hold environment variables for the collector module
     """
-    pass
+    settings = {}
+
+    def __init__(self):
+        try:
+            self.settings['reddit_secret'] = env['REDDIT_SECRET']
+            self.settings['reddit_id'] = env['REDDIT_ID']
+        except KeyError as e:
+            raise Exception(e)
+
+    @property
+    def reddit_secret(self):
+        return self.settings['reddit_secret']
+
+    @property
+    def reddit_id(self):
+        return self.settings['reddit_id']
