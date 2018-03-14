c986a14407ede5affb7c5b676bf696662f094e67
Andrew Koller <arellok@gmail.com>
2018-03-13 22:25:25 -0700

    Fixed variable name of reddit client

--- collector/reddit.py
+++ collector/reddit.py
@@ -15,11 +15,10 @@ def reddit_client():
         client_secret = config.reddit_secret,
         user_agent = 'crypto-nlp-api'
     )
-
     return reddit
 
 
-def get_posts(subreddit, style, limit):
-    subreddit = r.subreddit(subreddit)
+def get_posts(reddit_client, subreddit, style, limit):
+    subreddit = reddit_client.subreddit(subreddit)
     posts = getattr(subreddit, style)(limit=limit)
     return posts
