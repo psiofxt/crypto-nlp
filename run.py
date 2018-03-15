c6624a704ca71cfe792545ca623c15126ab8f7ae
Andrew Koller <arellok@gmail.com>
2018-03-14 23:21:57 -0700

    Updated run to include prelim reddit stuff

--- run.py
+++ run.py
@@ -4,6 +4,16 @@
 
 # local
 from classifier.train import get_classifier
+import collector.reddit as r
 
 
-classifier = get_classifier()
+# ----------------------------------------
+# REDDIT ---------------------------------
+# ----------------------------------------
+if __name__ == '__main__':
+    reddit = r.reddit_client()
+    posts = r.get_posts(reddit, 'cryptocurrency', 'hot', 5)
+    comments = r.get_all_comments(posts, 'bitcoin')
+    print(len(comments))
+
+    classifier = get_classifier()
