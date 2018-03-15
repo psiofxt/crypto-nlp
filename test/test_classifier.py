72c99f4b60cce959f519d10a27ecb7e654d9eb77
Andrew Koller <arellok@gmail.com>
2018-03-14 22:57:30 -0700

    Redefined logger config

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -1,6 +1,25 @@
 # stdlib
-import logging as logger
-logger.basicConfig(level=logger.INFO)
+import logging
+from logging.config import dictConfig
+
+logging_config = dict(
+    version = 1,
+    formatters = {
+        'f': {'format':
+              '%(asctime)s [%(levelname)s] %(message)s'}
+        },
+    handlers = {
+        'h': {'class': 'logging.StreamHandler',
+              'formatter': 'f',
+              'level': logging.INFO}
+        },
+    root = {
+        'handlers': ['h'],
+        'level': logging.INFO,
+        },
+)
+dictConfig(logging_config)
+logger = logging.getLogger()
 
 import sys
 from os import getcwd
