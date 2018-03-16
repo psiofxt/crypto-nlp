67e5721a83a03b20e8780e5139e92586af15de60
Andrew Koller <arellok@gmail.com>
2018-03-15 21:35:21 -0700

    Process data and write data functions added -- incomplete

--- classifier/train.py
+++ classifier/train.py
@@ -1,9 +1,11 @@
 # stdlib
+import json
 
 # third-party
 from textblob.classifiers import NaiveBayesClassifier
 
 # local
+from logs import logger
 
 
 def get_classifier():
@@ -23,3 +25,43 @@ def update_classifier(data, classifier):
     classifier.update(data)
 
     return classifier
+
+
+def process_data(data):
+    """
+    TODO: Docstring
+    """
+    for counter, entry in enumerate(data):
+        data[counter] = entry.replace('\n', '')
+
+    current = json.load(open('classifier/classifier.json'))
+
+    write = {}
+    for entry in data:
+        logger.info(entry)
+
+        while True:
+            value = input()
+            if value in ['pos', 'neg']:
+                break
+            logger.error('Enter either "pos" or "neg"')
+
+        write.update(
+            {
+                "text": entry,
+                "label": value
+            }
+        )
+
+    logger.info(write)
+    if len(write):
+        write_data(write)
+
+
+def write_data(update_data):
+    """
+    TODO: Docstring
+    """
+    with open('classifier/classifier.json', 'a') as data:
+        data.write(json.dumps(update_data))
+        data.close()
