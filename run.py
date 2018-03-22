ed81b52b8c0391f6e7fcef477ff4a48bf6eded1a
Andrew Koller <arellok@gmail.com>
2018-03-21 20:50:30 -0700

    Added versioneer then removed after tests
    Adding versioneer
    
    Removing parentdir
    
    removing versioneer

--- run.py
+++ run.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     reddit = r.reddit_client()
     posts = r.get_posts(reddit, 'cryptocurrency', 'hot', 5)
     comments = r.get_all_comments(posts, 'bitcoin')
-    print(len(comments))
+    #print(len(comments))
 
     classifier = get_classifier()
