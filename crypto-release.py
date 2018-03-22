d9bbeebc5de0278236d69172ff97ac9837aedb1d
Andrew Koller <arellok@gmail.com>
2018-03-22 14:48:47 -0700

    test

--- crypto-release.py
+++ crypto-release.py
@@ -1,6 +1,16 @@
 import argparse
+from subprocess import check_output
 
 from git import Repo
+import semantic_version
+
+
+def get_version_from_git():
+    label = check_output(
+        ['git', 'describe', 'version-test', '--tags']
+    )
+    label = label.strip().decode('UTF-8')
+    return label if semantic_version.validate(label) else False
 
 
 if __name__ == '__main__':
@@ -9,15 +19,35 @@ if __name__ == '__main__':
     parser.add_argument('-ma', '--major', action='store_true')
     parser.add_argument('-p', '--patch', action='store_true')
     parser.add_argument('-a', '--message', type=str)
-    parser.add_argument('-t', '--tag', type=str)
-    args = parser.parse_args()
+    args = vars(parser.parse_args())
+    git_version = get_version_from_git()
 
-    if not (args.minor and args.major and args.patch):
-        args.minor = True
+    increment = {k: args[k] for k in args if args[k]}
 
-    if args.message:
-        repo = Repo()
-        git = repo.git
+    # No version arguments supplied. Defaulting to "patch" 0.0.x
+    if not len(increment):
+        git_version = semantic_version.Version(git_version)
+        new_version = git_version.next_patch()
+
+    # One version argument supplied. Either "major": x.0.0 or "minor": 0.x.0
+    elif len(increment) == 1:
+        git_version = semantic_version.Version(git_version)
+        new_version = getattr(git_version, 'next_' + list(increment.keys())[0])()
+        print(new_version)
+
+    # Multiple version arguments supplied. Unsupported
+    else:
+        raise Exception("Multiple version arguments detected." +
+                        " Please supply one or none to default to patch")
+
+    repo = Repo()
+    git = repo.git
+    if args['message']:
         git.add('-A')
-        git.commit('-m {}'.format(args.message))
+        git.commit('-m {}'.format(args['message']))
         git.push()
+        git.tag(new_version)
+        git.push('origin tags')
+    else:
+        git.tag(new_version)
+        git.push('origin --tags')
--- setup.py
+++ setup.py
@@ -1,3 +1,61 @@
-import setuptools
+from distutils.core import setup
+from subprocess import check_output
+from crypto-release import get_version_from_git
 
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+version = get_version_from_git()
+if not version:
+    raise Exception("Version formatting incompatibility")
+
+setup(
+    # Application name:
+    name="crypto-nlp",
+
+    # Version number (initial):
+    version=version,
+
+    # Application author details:
+    author="ak",
+    author_email="",
+
+    # Packages
+    packages=["crypto-nlp"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="ankoller.com",
+
+    #
+    # license="LICENSE.txt",
+    description="nlp of crypto language",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "appnope==0.1.0",
+        "attrs==17.4.0",
+        "decorator==4.2.1",
+        "python-dotenv==0.8.2",
+        "ipython==6.2.1",
+        "ipython-genutils==0.2.0",
+        "jedi==0.11.1",
+        "nltk==3.2.5",
+        "parso==0.1.1",
+        "pexpect==4.4.0",
+        "pickleshare==0.7.4",
+        "pluggy==0.6.0",
+        "praw==5.3.0",
+        "prompt-toolkit==1.0.15",
+        "ptyprocess==0.5.2",
+        "py==1.5.2",
+        "Pygments==2.2.0",
+        "pytest==3.4.2",
+        "simplegeneric==0.8.1",
+        "six==1.11.0",
+        "textblob==0.15.1",
+        "traitlets==4.3.2",
+        "wcwidth==0.1.7",
+    ],
+)
